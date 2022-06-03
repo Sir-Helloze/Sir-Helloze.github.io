736ebbecefd57e0c1b1b4227f1f32d6e40a8d5cb
Sir-Helloze <babywalk@mail.ru>
2022-06-03 15:31:22 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,28 +21,34 @@ function newElement(){
         if (object !=0 ){
             console.log(object);
             var out = '';
-            out +=`<div class="task"><ul><li><p>${object.name}</p><button class="load" onclick="load()">load</button></li><ul><li><p>${object.discr} --- ${object.time} H </p></li></ul></ul></div>`;
+            out +=`<div class="task"><ul><li><p>${object.name}</p><button class="load" onclick="load()">load</button></li><ul><li><p>${object.discr} - ${object.time} H </p></li></ul></ul></div>`;
             $('#list').html(out);
             localStorage.setItem('save1', document.getElementById('list').innerHTML);
             
         }
     }
     else{
-        alert("error")
+        alert("вы не заполнили поля для ввода")
     }
     toSave()
+    load()
 }
 function toSave(){
     var save = localStorage.getItem('save1');
     document.getElementById('list').innerHTML = save;
-    
 }
 toSave()
 function load(){
-    localStorage.getItem('todos', object);
+    var ex = JSON.parse(localStorage.getItem('todos'));
     var outSub = '';
-    out +=`<tr><td><input type="text" placeholder="Old Task" id="oldTask" value="${object.discr}"></td><td><input id="timeTask" value="${object.time}"></td><td><button id="delTask">delete</button></td></tr>`;
+    document.querySelector("#nameTask").value = ex.name;
+    outSub +=`<tr><td><input type="text" placeholder="Old Task" id="oldTask" value="${ex.discr}"></td><td><input id="timeTask" value="${ex.time}"></td><td><button id="delTask">delete</button></td></tr>`;
     $('#subTask').html(outSub);
+}
+function addSub(){
+    var addSub = '';
+    addSub +=`<tr><td><input type="text" placeholder="New Task" id="newTask"></td><td><input id="timeTask"></td><td><button id="delTask">delete</button></td></tr>`;
+    $('#addSub').html(addSub);
 }
 
     
